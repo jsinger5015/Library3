111a4f380f5eb2bd9c1f5f083eede8f0bc6f7815
jsinger5015 <jsinger5015@001LAB102010532.neisd.ad>
2015-05-01 16:04:31 -0500

    fasdg

---
+++ Library3/src/People.java
@@ -0,0 +1,12 @@
+public interface People {
+	
+	public long getID();
+	public void setID();
+	
+
+	public void setName(String fname);
+	public String getFName();
+	
+	public String setName(String fname);
+	public String setName(String fname);
+}
