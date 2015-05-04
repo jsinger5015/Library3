c7b402834e979a846584d27cf45523ddee069ae2
jsinger5015 <jsinger5015@001LAB102010532.neisd.ad>
2015-05-04 15:24:54 -0500

    fgn

--- Library3/src/Item.java
+++ Library3/src/Item.java
@@ -1,4 +1,14 @@
 
 public interface Item {
 
+	public boolean isCheckedIn = true; 
+	
+	public String Title;
+	
+	
+	//public void implementsitem(); //books, movies, music 
+	
+	public void people(); 
+	//within have class for libraries and customers for selfcheckout
+
 }
---
+++ Library3/src/customer.java
@@ -0,0 +1,32 @@
+
+public class customer implements People {
+
+	private long ID;
+	private String fName;
+	private String lName;
+	
+	@Override
+	public long getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setID() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String setName(String fname) {
+		// TODO Auto-generated method stub
+		fName = fname;
+	}
+
+	@Override
+	public String getFName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
